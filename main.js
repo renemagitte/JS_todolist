341583b7783e69468638a6b55b535ae9c596e58e
renemagitte <Ida@Idas-MBP.zyxel.com>
2018-02-09 21:40:04 +0100

    removes all tasks/children within div/parent

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const addTaskInput = document.getElementById('addTaskInput');
 const addTaskButton = document.getElementById('addTaskButton');
 const unmadeTaskList = document.getElementById('unmadeTaskList');
+const removeUnmadeTasksButton = document.getElementById('removeUnmadeTasksButton');
+
+
+removeUnmadeTasksButton
 
 
 addTaskButton.addEventListener('click', function(){
@@ -12,11 +16,20 @@ addTaskButton.addEventListener('click', function(){
     const div = document.createElement('div');
     //document.body.appendChild(div);
     unmadeTaskList.appendChild(div);
-    div.innerText = addTaskInput.value;
+    div.innerText = addTaskInput.value; 
+});
 
 
+removeUnmadeTasksButton.addEventListener('click', function(){
+    //unmadeTaskList.removeChild;
+    //unmadeTaskList.removeChild(unmadeTaskList.firstChild)
+    
+    while(unmadeTaskList.hasChildNodes()){
+        unmadeTaskList.removeChild(unmadeTaskList.lastChild)
+    }
     
 });
+                               
 
 //addTaskInput.addEventListener('submit', function(){
 //    event.preventDefault();
