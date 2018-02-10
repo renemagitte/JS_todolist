0052eb0e4edb15ff8dcd46122789548e23983a98
renemagitte <Ida@Idas-MBP.zyxel.com>
2018-02-10 15:55:52 +0100

    progress with doubletcheck

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const addTaskButton = document.getElementById('addTaskButton');
 const unmadeTaskList = document.getElementById('unmadeTaskList');
 const removeUnmadeTasksButton = document.getElementById('removeUnmadeTasksButton');
 var storageOfCurrentlyMovingTask;
+var arrayOfTasks = [];
 
 
 const completedTaskList = document.getElementById('completedTaskList');
@@ -16,10 +17,15 @@ addTaskButton.addEventListener('click', function(){
     const singleTaskWrapper = document.createElement('div');
     const checkDiv = document.createElement('div');
     const taskDiv = document.createElement('div');
-    
+ 
     const removeSingleTaskButton = document.createElement("button"); //måste sätta value på denna knapp???
     
+    //lägga in i array med listpunkter
 
+    
+    
+    doubletCheck(addTaskInput.value);
+    
     singleTaskWrapper.classList.add('singleTaskWrapper');
     checkDiv.classList.add('checkDiv');
     taskDiv.classList.add('taskDiv');
@@ -32,14 +38,14 @@ addTaskButton.addEventListener('click', function(){
     taskDiv.innerHTML = addTaskInput.value; 
     taskDiv.appendChild(removeSingleTaskButton); 
     
+    arrayOfTasks.push(addTaskInput.value);
+    
     addTaskInput.value = ''; // clearing input field again after current task has been added
     
 // Checking off a task and moving it to complete list
         checkDiv.addEventListener('click', function(){
             completedTaskList.appendChild(singleTaskWrapper);
-            
-                this.innerHTML = '<span class="glyphicon glyphicon-glyphicon glyphicon-heart" aria-hidden="true"></span> ';
-            
+            this.innerHTML = '<span class="glyphicon glyphicon-glyphicon glyphicon-heart" aria-hidden="true"></span> ';
         });          
 
             
@@ -92,6 +98,46 @@ removeUnmadeTasksButton.addEventListener('click', function(){
 });
     
 }) // end of addTask(Button)
+
+
+                                function doubletCheck(taskToCheck){
+                                    for (i = 0; i < arrayOfTasks.length; i++) {
+                                            if(taskToCheck === arrayOfTasks[i]){
+                                                  console.log("Denna uppgift finns redan!");
+                                            }else{
+                                                    console.log("ok task");
+                                            }
+                                    }
+                    
+                                }
+
+
+//                                function doubletCheck(taskToCheck){
+//                                    //counting rows in task list for preparing for loop.
+//                                    
+//NodeList.prototype.forEach = Array.prototype.forEach
+//var children = unmadeTaskList.childNodes;
+//children.forEach(function(item){
+//    console.log(item);
+//});
+////                                    var allTasks = document.getElementById("unmadeTaskList").childElementCount + 1;
+////                                       
+////                                    //console.log(allTasks);
+////                                    console.log(taskDiv.innerText);
+////                                    
+////                                    for (i = 0; i < allTasks.length; i++) {
+////                                        
+////                                        console.log(taskDiv.innerText);
+////                                        
+//////                                            if(taskToCheck === taskDiv.innerText){
+//////                                                console.log("Denna uppgift finns redan!");
+//////                                            }else{
+//////                                                console.log("ok task");
+//////                                            }
+////                                    }
+//                                    
+//                              
+//                                }
                                
 
 //addTaskInput.addEventListener('submit', function(){
